f92435ab211ca5ca1e78f76328914664586343e7
adeelh1 <adeel.haidery87@gmailcom>
2017-07-29 16:32:26 +0530

    updated fibo

---
+++ date.py
@@ -0,0 +1,1 @@
+#Write a date function#
--- hello.py
+++ hello.py
@@ -1,3 +1,9 @@
+#import date
+#import function
+
 print "Good Morning"
 
 print "Hi"
+
+#call the function from date.py and print date
+#call the function in fibo series and print date
